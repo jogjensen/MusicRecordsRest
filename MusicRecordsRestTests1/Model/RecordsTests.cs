d3dae11ed59a317c3b7b64a289d54c4c4c4cb17a
Nikolai Køhler Lundberg Hansen <niko9946@edu.easj.dk>
2020-10-20 12:42:41 +0200

    Nikolaj pusher

--- MusicRecordsRestTests1/Model/RecordsTests.cs
+++ MusicRecordsRestTests1/Model/RecordsTests.cs
@@ -3,13 +3,21 @@ using MusicRecordsRest.Model;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MusicRecordsRest.Controllers;
 
 namespace MusicRecordsRest.Model.Tests
 {
     [TestClass()]
     public class RecordsTests
     {
+        private RecordsController _record = new RecordsController();
         private Records records = new Records(1, "Niqulay", "Niko", 2000, 5);
+
+        //[TestInitialize]
+        //public ShowAllRestTest()
+        //{
+        //    records = new Records();
+        //}
 
         //Constructor test
         [TestMethod()]
@@ -22,6 +30,13 @@ namespace MusicRecordsRest.Model.Tests
             Assert.AreEqual("Niko", "Niko");
             Assert.AreEqual(2000, 2000);
             Assert.AreEqual(5, 5);
+        }
+
+        //tester listen fa crontrolleren
+        [TestMethod()]
+        public void RecordGetList()
+        { 
+
         }
         
 
