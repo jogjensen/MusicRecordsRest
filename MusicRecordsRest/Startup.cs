9ee47cbde30c974e50e1877a25ba609801a73e14
Philip Lind Jensen <jogjensen@hotmail.com>
2020-10-20 11:28:39 +0200

    Added swagger

--- MusicRecordsRest/Startup.cs
+++ MusicRecordsRest/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MusicRecordsRest
 {
@@ -25,7 +26,14 @@ namespace MusicRecordsRest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Records API", Version = "v1.0" });
+            });
         }
+        
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -43,6 +51,11 @@ namespace MusicRecordsRest
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Items API v1.0")
+            );
         }
     }
 }
---
+++ MusicRecordsRestTests1/Model/RecordsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MusicRecordsRest.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicRecordsRest.Model.Tests
+{
+    [TestClass()]
+    public class RecordsTests
+    {
+        private Records records = new Records(1, "Niqulay", "Niko", 2000, 5);
+
+        //Constructor test
+        [TestMethod()]
+        public void RecordsConstructorTest()
+        {
+            Records records = new Records(1, "Niqulay", "Niko", 2000, 5);
+
+            Assert.AreEqual(1, 1);
+            Assert.AreEqual("Niqulay", "Niqulay");
+            Assert.AreEqual("Niko", "Niko");
+            Assert.AreEqual(2000, 2000);
+            Assert.AreEqual(5, 5);
+        }
+        
+
+    }
+}
